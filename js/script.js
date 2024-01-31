50de8f2f7bbc9ac93981b3d0c3bddf46f525903a
post-coder <gabrielspanu.dev@gmail.com>
2024-01-31 12:33:30 +0100

    manipolazione dom con il ciclo for

--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
 
+// mi memorizzo la lista
+const listaElement = document.querySelector(".lista");
 
-// mostrare tutti i numeri pari dallo 0 al 10
+// aggiungo dieci elementi alla lista
+for(let i = 0; i < 10; i++) {
 
-// ciclo che scorre il valore di i da 0 a 10 (11 iterazioni)ù
-console.log("stampiamo i numeri pari da 0 a 10")
-for (let i = 0; i <= 10; i++) {
-    
-    // controllo che il resto della divisione per 2 sia zero
-    // (quindi il numero per definizione è pari)
-    if(i % 2 == 0) {
+    listaElement.innerHTML += `<li>${i}</li>`
 
-        console.log(i);
-        
-    }
-
-}
-
-
-console.log("stampiamo i multipli di 3")
-
-// proviamo a riscrivere il nostro ciclo for
-// cambiando il modo in cui AGGIORNIAMO il valore di i
-
-for (let i = 4; i <= 30; i += 3) {
-    console.log(i);
 }
