3d93ab908ec67a9cdd75246953cd19abf87f1c39
post-coder <gabrielspanu.dev@gmail.com>
2024-01-31 12:58:23 +0100

    griglia di elementi

--- js/script.js
+++ js/script.js
@@ -4,10 +4,28 @@
 const grigliaElement = document.querySelector("#griglia");
 
 
-for(let i = 0; i <= 100; i++) {
-     
-    grigliaElement.innerHTML += `
-        <div class="square" >${i}</div>
-    `;
+for(let i = 0; i < 1000 ; i++) {
+
+    // creare un elemento html
+    const newElement = document.createElement("div");
+    
+    // una volta che creiamo un elemento, possiamo modificarlo successivamente
+    // come vogliamo
+    newElement.className = "square";
+    newElement.innerHTML = i;
+    newElement.style.color = "blue";
+    newElement.style.fontWeight = "bold";
+    newElement.style.fontSize = "24px";
+
+    // facciamo il controllo se la i è multipla di due (pari)
+    if(i % 2 == 0) {
+        newElement.className += " even";
+    }
+ 
+    // alla fine "appendiamo" l'elemento appena creato (newElement)
+    // alla griglia (grigliaElement)
+    // il metodo .append() aggiunge all'interno di grigliaElement
+    // l'elemento che gli passiamo tra parentesi, esso sarà l'ultimo figlio
+    grigliaElement.append(newElement);
 
 }
